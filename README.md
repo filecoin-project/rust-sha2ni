7ea49428dfb881c7f0e9684437962e7e91d6cca7
dignifiedquire <me@dignifiedquire.com>
2020-04-27 12:37:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sha2Ni
+
+> Sha2 implemention in Rust, aimed at performance. 
+> Forked from [Sha2](https://github.com/rustcrypto/hashes).
+
+
+## License
+
+Dual-licensed under Apache 2.0 and MIT terms:
+
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
---
+++ src/platform.rs
@@ -0,0 +1,89 @@
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Platform {
+    Portable,
+    #[cfg(feature = "asm")]
+    Asm,
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    Sha,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Implementation(Platform);
+
+impl Implementation {
+    pub fn detect() -> Self {
+        // Try the different implementations in order of how fast/modern they are.
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            if let Some(sha_impl) = Self::sha_if_supported() {
+                return sha_impl;
+            }
+        }
+        #[cfg(any(feature = "asm", feature = "asm-aarch64"))]
+        {
+            if let Some(asm_impl) = Self::asm_if_supported() {
+                return asm_impl;
+            }
+        }
+
+        Self::portable()
+    }
+
+    pub fn portable() -> Self {
+        Implementation(Platform::Portable)
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[allow(unreachable_code)]
+    pub fn sha_if_supported() -> Option<Self> {
+        use raw_cpuid::CpuId;
+
+        // Use raw_cpuid instead of is_x86_feature_detected, to ensure the check
+        // never happens at compile time.
+        let cpuid = CpuId::new();
+        let is_runtime_ok = cpuid
+            .get_extended_feature_info()
+            .map(|info| info.has_sha())
+            .unwrap_or_default();
+
+        // Make sure this computer actually supports it
+        if is_runtime_ok {
+            return Some(Implementation(Platform::Sha));
+        }
+
+        None
+    }
+
+    #[cfg(any(feature = "asm", feature = "asm-arch64"))]
+    pub fn asm_if_supported() -> Option<Self> {
+        #[cfg(feature = "asm-aarch64")]
+        let supported = ::aarch64::sha2_supported();
+        #[cfg(not(feature = "asm-aarch64"))]
+        let supported = false;
+
+        if supported {
+            return Some(Implementation(Platform::Asm));
+        }
+        None
+    }
+
+    #[inline]
+    pub fn compress256(&self, state: &mut [u32; 8], block: &[u8; 64]) {
+        match self.0 {
+            Platform::Portable => {
+                use sha256_utils;
+                sha256_utils::compress256(state, block);
+            }
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            Platform::Sha => {
+                use sha256_intrinsics;
+                unsafe { sha256_intrinsics::compress256(state, block) };
+            }
+            #[cfg(feature = "asm")]
+            Platform::Asm => {
+                sha2_asm::compress256(state, block);
+            }
+        }
+    }
+}
